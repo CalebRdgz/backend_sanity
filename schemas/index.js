e41498858bb57bdd4398eb1f76e6fc0bc4ff440a
Caleb <calebrod1@yahoo.com>
2023-02-05 22:14:56 -0600

    backend commits

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import testimonials from "./testimonials"
+import abouts from "./abouts"
+import works from "./works"
+import skills from "./skills"
+import workexperience from "./workExperience"
+import experiences from "./experiences"
+import contact from "./contact"
+
+export const schemaTypes = [testimonials, abouts, works, skills, workexperience, experiences, contact]
